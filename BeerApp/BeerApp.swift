18ce377f8dd799c6eaf5a2cd7b5a010a985c0563
Ilya Biltuev <ilyabiltuev@gmail.com>
2025-01-30 22:00:16 +0300

    added ios project files

---
+++ BeerApp/BeerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeerAppApp.swift
+//  BeerApp
+//
+//  Created by Илья Билтуев on 30.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct BeerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BeerApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BeerApp
+//
+//  Created by Илья Билтуев on 30.01.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
